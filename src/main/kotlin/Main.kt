52e9ee49f619ec71f1ae9d3f65e62b0f761fa942
Alexey <alexey.khachatryan@gmail.com>
2022-10-16 15:50:17 +0400

    Using recursion handle math statement and do calculations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,89 @@
-import io.kaen.dagger.ExpressionParser
-
-fun main() {
+fun main(args: Array<String>) {
     println("Type the calculating operation: ")
+    print(resolveOperation(readln()))
+}
 
-    val parser = ExpressionParser()
-    val result = parser.evaluate(readln())
-    println(result)
+fun resolveOperation(operationsStr: String): Float {
+    val numbers = operationsStr.split('+', '-', '/', '*').toMutableList()
+    val operations = operationsStr.split(regex = Regex("""\d+""")).toMutableList()
+
+    val operationsWithHighPrior = listOf("*", "/");
+    val operationsWithLowPrior = listOf("+", "-");
+    var modifiedStatement = ""
+    operations.removeIf { it == "" || it == "."}
+
+    for(i in operations.indices) {
+        val op = operations[i];
+
+        if(! operationsWithHighPrior.contains(op)) {
+            continue
+        }
+
+        val firstNumIndex = i
+        val secondNumIndex = i + 1
+        var result = 0F
+
+        if(op == "*") {
+            result = numbers[firstNumIndex].toFloat() * numbers[secondNumIndex].toFloat()
+        }
+        else if(op == "/") {
+            result = numbers[firstNumIndex].toFloat() / numbers[secondNumIndex].toFloat()
+        }
+
+        numbers[firstNumIndex] = result.toString()
+        numbers.removeAt(secondNumIndex)
+        operations.removeAt(i)
+
+        return if(operations.size == 0) {
+            result
+        } else {
+            for (numberIndex in numbers.indices) {
+                modifiedStatement += numbers[numberIndex]
+
+                if(numberIndex + 1 != numbers.size) {
+                    modifiedStatement += operations[numberIndex]
+                }
+            }
+
+            resolveOperation(modifiedStatement)
+        }
+    }
+
+    for(i in operations.indices) {
+        val op = operations[i];
+        if(! operationsWithLowPrior.contains(op)) {
+            continue
+        }
+        val firstNumIndex = i
+        val secondNumIndex = i + 1
+        var result = 0F
+
+
+        if(op == "+") {
+            result = numbers[firstNumIndex].toFloat() + numbers[secondNumIndex].toFloat()
+        }
+        else if(op == "-") {
+            result = numbers[firstNumIndex].toFloat() - numbers[secondNumIndex].toFloat()
+        }
+
+        numbers[firstNumIndex] = result.toString()
+        numbers.removeAt(secondNumIndex)
+        operations.removeAt(i)
+
+        return if(operations.size == 0) {
+            result
+        } else {
+            for (numberIndex in numbers.indices) {
+                modifiedStatement += numbers[numberIndex]
+
+                if(numberIndex + 1 != numbers.size) {
+                    modifiedStatement += operations[numberIndex]
+                }
+            }
+
+            resolveOperation(modifiedStatement)
+        }
+    }
+
+    return 0F
 }
